652ce3ddb140a1d9d5722487d828dcef2a26bcb6
AndreiTarce <tarceandrei@gmail.com>
2022-03-25 12:38:22 +0200

    Updates

---
+++ precache-manifest.3c036b3c5aba52d26af7e2bdeb598298.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "97a70f2cfefba8e4477e",
+    "url": "/react-rotaract-website/static/css/main.c6085106.chunk.css"
+  },
+  {
+    "revision": "97a70f2cfefba8e4477e",
+    "url": "/react-rotaract-website/static/js/main.90d0de43.chunk.js"
+  },
+  {
+    "revision": "0e2dd1deb0505504e08a",
+    "url": "/react-rotaract-website/static/js/runtime~main.34875645.js"
+  },
+  {
+    "revision": "f57bc92fe893daa00db8",
+    "url": "/react-rotaract-website/static/js/2.c1226526.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/react-rotaract-website/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "d1b4cce8b9fade9ed6eed70ed57d89a1",
+    "url": "/react-rotaract-website/static/media/club2.d1b4cce8.jpg"
+  },
+  {
+    "revision": "2ec572dff0d5bb2816de54ae13c7c1c9",
+    "url": "/react-rotaract-website/static/media/visio.2ec572df.png"
+  },
+  {
+    "revision": "951617c69cde53103e642296945690de",
+    "url": "/react-rotaract-website/index.html"
+  }
+];
